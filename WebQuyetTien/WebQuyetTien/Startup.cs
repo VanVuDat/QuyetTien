b6faf408b728d3c4d8d18f25c2aab4859068e10e
US <finnalj3@gmail.com>
2018-11-21 10:46:14 +0700

    empty project

---
+++ WebQuyetTien/WebQuyetTien/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebQuyetTien.Startup))]
+namespace WebQuyetTien
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
